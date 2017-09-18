0c07b8c5f60db663f8e939e099b57164a9d2bbad
Louie Arencibia <louie728@gmail.com>
2017-09-18 10:02:20 -0400

    init Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mldata::Application.config.secret_token = '29dc1c22915ead7b2e204b55df91d06add1227d2895b228a4ed64edbe43d55ea51e871d819758b9b1fb88a7298ebcbbf7dafff3b912133c3a7a0795561df75b9'
