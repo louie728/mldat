bfb91286f792e5788bb27c321eece3c63b2973e7
Louie Arencibia <louie728@gmail.com>
2017-09-18 15:57:36 -0400

    import script

--- app/models/imp_frmt.rb
+++ app/models/imp_frmt.rb
@@ -1,5 +1,5 @@
 class ImpFrmt < ActiveRecord::Base
-
-  attr_accessible   :title, :isbn, :book_code, :series, :series_code , :price, :lib_price , :copyright_date , :pub_marketing, :flip_isbn
+  attr_accessible *column_names  # :title, :isbn, :book_code, :series, :series_code , :price, :lib_price , :copyright_date , :pub_marketing, :flip_isbn,
+                   # :dewey
 
 end
--- script/create_import_file.rb
+++ script/create_import_file.rb
@@ -8,8 +8,8 @@ ImpFrmt.delete_all
 
 
 Book.all.each do |b|
-  puts "Creating and import record for #{b.title} : #{b.ISBN13}"
-  ebook_isbn = (b.eb_ISBN13.nil?) ?   '' : b.eb_ISBN13
+  #puts "Creating an import record for #{b.title} : #{b.ISBN13}"
+  ebook_isbn = (b.eb_ISBN13.nil? || b.eb_ISBN13 == 'NULL' ) ?   '' : b.eb_ISBN13
   ImpFrmt.create(
    {
     title: b.title,
@@ -24,4 +24,76 @@ Book.all.each do |b|
     flip_isbn: ebook_isbn
    }
   )
+end
+
+#updating import file series names
+Serie.all.each do |s|
+  #puts "Updating book series names for #{s.ID} - #{s.title} "
+  series_books =  ImpFrmt.find_all_by_series_code(s.ID)
+  series_books.each do |sb|
+    sb.update_attribute(:series, s.title)
+  end
+end
+
+# Go through export from esebco and fill ou the info or create
+# record if needed
+MlExp.all.each do |mlb|
+  imp_rec = ImpFrmt.find_by_isbn(mlb.number)
+  dewey_number = (mlb.dewey_number.to_s.strip == '0.0' || mlb.dewey_number.to_s.strip == '0') ? '' : mlb.dewey_number
+  if imp_rec
+    copy_year = (imp_rec.copyright_date.empty? || imp_rec.copyright_date == 'NULL' ) ? mlb.copyright_year : imp_rec.copyright_date
+    imp_rec.update_attributes(
+      copyright_date: copy_year,
+      dewey: dewey_number,
+      author: mlb.author,
+      int_level: mlb.int_lvls,
+      read_level: mlb.rd_lvls,
+      length: mlb.page_count,
+      binding: mlb.media_type,
+      lex: mlb.lexile_measure,
+      bisac_cats: mlb.subjects,
+      atos_l: mlb.atos_level ,
+      atos_int_lvl: mlb.ar_int ,
+      atos_p: mlb.ar_points ,
+      quiz: mlb.ar_quiz,
+      rc_lvl: mlb.rc_level ,
+      rc_pnts: mlb.rc_points,
+      grl: mlb.grl
+
+
+    )
+  else
+    puts "Esebco has #{mlb.title} - #{mlb.number} and mlane doesn't"
+    if !mlb.title.empty?
+      ImpFrmt.create(
+          {
+              title: mlb.title,
+              isbn: mlb.number,
+              book_code: '',
+              series: '' ,
+              series_code: '',
+              price: '',
+              lib_price: '',
+              copyright_date: mlb.copyright_year,
+              pub_marketing: mlb.description,
+              dewey: dewey_number,
+              author: mlb.author,
+              int_level: mlb.int_lvls,
+              read_level: mlb.rd_lvls,
+              length: mlb.page_count,
+              binding: mlb.media_type,
+              lex: mlb.lexile_measure,
+              bisac_cats: mlb.subjects,
+              atos_l: mlb.atos_level ,
+              atos_int_lvl: mlb.ar_int ,
+              atos_p: mlb.ar_points ,
+              quiz: mlb.ar_quiz,
+              rc_lvl: mlb.rc_level ,
+              rc_pnts: mlb.rc_points,
+              grl: mlb.grl
+
+          }
+      )
+    end
+  end
 end
